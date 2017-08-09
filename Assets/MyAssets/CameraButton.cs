2b9252fee5932f96e8cf956bf1b09f4dc976e768
林健太郎 <hayashikentarou@hayashikentarounoMacBook-Pro.local>
2017-08-09 16:01:37 +0900

    add take button

---
+++ Assets/MyAssets/CameraButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraButton : MonoBehaviour {
+	public GameObject button;
+	// Use this for initialization
+	void Start () {
+		button = GameObject.Find("Button");
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TakeShot);
+	}
+	
+	void TakeShot(){}
+}
