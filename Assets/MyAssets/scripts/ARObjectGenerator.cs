9887611c42aecfa0e30d85f343c8f671b8e57059
hayaken8112 <hayashi8112@gmail.com>
2017-08-30 17:41:40 +0900

    resource/Prefabからロードして、オブジェクトを切り替えれるようにした

--- Assets/MyAssets/scripts/ARObjectGenerator.cs
+++ Assets/MyAssets/scripts/ARObjectGenerator.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UniRx;
+using UnityEditor;
 
 namespace UnityEngine.XR.iOS
 {
 	public class ARObjectGenerator : MonoBehaviour
 	{
-		public GameObject prefab;
+		Object[] prefabs;
+		Stack<Object> ARObjectStack = new Stack<Object>();
+		Button btn1;
+		Button btn2;
+		Button undoBtn;
+		int nextARObjIndex = 0; 
 		bool isOnGameObject = true;
-		bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+		bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes, int prefabIndex)
 		{
 			List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
 			if (hitResults.Count > 0) {
@@ -18,7 +25,7 @@ namespace UnityEngine.XR.iOS
 					Quaternion rot;
 					pos = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
 					rot = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
-					Instantiate (prefab, pos, rot);
+					ARObjectStack.Push(Instantiate (prefabs[prefabIndex], pos, rot));
 					Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", pos.x, pos.y, pos.z));
 					return true;
 				}
@@ -29,6 +36,16 @@ namespace UnityEngine.XR.iOS
 		bool isTouched() {
 			if (Input.touchCount > 0) return true;
 			else return false;
+		}
+		void Start() {
+			btn1 = GameObject.Find("ObjectButton1").GetComponent<Button>();
+			btn2 = GameObject.Find("ObjectButton2").GetComponent<Button>();
+			undoBtn = GameObject.Find("UndoButton").GetComponent<Button>();
+
+			btn1.OnClickAsObservable().Subscribe(_ => nextARObjIndex = 0);
+			btn2.OnClickAsObservable().Subscribe(_ => nextARObjIndex = 1);
+			undoBtn.OnClickAsObservable().Subscribe(_ => Destroy(ARObjectStack.Pop()));
+			prefabs = Resources.LoadAll("Prefab"); // Resources/Prefab内のPrefabをロード
 		}
 /*
 		void start () {
@@ -69,7 +86,9 @@ namespace UnityEngine.XR.iOS
 		*/
 		// Update is called once per frame
 		void Update () {
-			if (Input.touchCount > 0 && prefab != null )
+			RawImage btnImage1 = GameObject.Find("ObjectButton1").GetComponent<RawImage>();
+			btnImage1.texture =  AssetPreview.GetAssetPreview(prefabs[0]);
+			if (Input.touchCount > 0)
 			{
 				isOnGameObject = EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
 				if (isOnGameObject == false){
@@ -94,7 +113,7 @@ namespace UnityEngine.XR.iOS
 
 					foreach (ARHitTestResultType resultType in resultTypes)
 					{
-						if (HitTestWithResultType (point, resultType))
+						if (HitTestWithResultType (point, resultType, nextARObjIndex))
 						{
 							return;
 						}
