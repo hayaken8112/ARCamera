2458d2aec944ef6e9f52d67deb896e41c557b3ae
hayaken8112 <hayashi8112@gmail.com>
2017-09-10 17:58:45 +0900

    inputFieldが最初から表示されないようにした。

---
+++ Assets/MyAssets/scripts/MyInputField.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class MyInputField : MonoBehaviour {
+
+	InputField inputField;
+	string inputText;
+	
+	// Use this for initialization
+	void Start () {
+		inputField = this.GetComponent<InputField>();	
+		inputField.ActivateInputField();
+		inputText = "";
+		ARCamera.TextObjectGenarator.Instance.textObject =  FlyingText.GetObject(inputText, new Vector3(0,0,1), Quaternion.identity);
+		inputField.OnValueChangedAsObservable()
+		.Subscribe( text => 
+		{
+			inputText = text;
+			Debug.Log(text);
+			FlyingText.UpdateObject(ARCamera.TextObjectGenarator.Instance.textObject, inputText);
+		});
+		inputField.OnEndEditAsObservable()
+		.Where(text => !String.IsNullOrEmpty(text))
+		.Subscribe(_ => {
+			ARCamera.ARObjectGenerator.Instance.kindOfnextObject = ARCamera.ARObjectGenerator.KindOfObject.Text;
+			ARCamera.ARObjectGenerator.Instance.nextARObject = ARCamera.TextObjectGenarator.Instance.textObject;
+			/*
+			textObject = FlyingText.GetObject("" , new Vector3(0,0,1) , Quaternion.identity);
+			inputField.text = "";
+			*/
+			Destroy(this.gameObject);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/MyAssets/scripts/TextObjectGenarator.cs
+++ Assets/MyAssets/scripts/TextObjectGenarator.cs
@@ -6,33 +6,26 @@ using UnityEngine.UI;
 using UniRx;
 using UniRx.Triggers;
 
-public class TextObjectGenarator : MonoBehaviour {
+namespace ARCamera {
+public class TextObjectGenarator : SingletonMonoBehaviour<TextObjectGenarator> {
 
+	GameObject canvas;
+	public GameObject inputFieldPrefab;
+	GameObject inputFieldInstance;
 	Button textButton;
-	InputField inputField;
-	string inputText = "";
-	GameObject textObject;
+	public GameObject textObject { get; set; }
+	private Subject<GameObject> TextObjectSubject = new Subject<GameObject>();
+	public IObservable<GameObject> OnTextObjectGenerated
+	{
+		get { return TextObjectSubject; }
+	}
 	// Use this for initialization
 	void Start () {
+		canvas = GameObject.Find("Canvas");
 		textButton = GameObject.Find("TextButton").GetComponent<Button>();
 		textButton.OnClickAsObservable().Subscribe(_ => {
-			inputField.ActivateInputField();
-			textObject =  FlyingText.GetObject(inputText, new Vector3(0,0,1), Quaternion.identity);
-		});
-		inputField = GameObject.Find("InputField").GetComponent<InputField>();
-		inputField.OnValueChangedAsObservable()
-		.Subscribe( text => 
-		{
-			inputText = text;
-			FlyingText.UpdateObject(textObject, inputText);
-		});
-		inputField.OnEndEditAsObservable()
-		.Where(text => !String.IsNullOrEmpty(text))
-		.Subscribe(_ => {
-			ARCamera.ARObjectGenerator.Instance.kindOfnextObject = ARCamera.ARObjectGenerator.KindOfObject.Text;
-			ARCamera.ARObjectGenerator.Instance.nextARObject = textObject;
-			textObject = FlyingText.GetObject("" , new Vector3(0,0,1) , Quaternion.identity);
-			inputField.text = "";
+			inputFieldInstance = Instantiate(inputFieldPrefab);
+			inputFieldInstance.transform.SetParent(canvas.transform, false);
 		});
 	}
 	
@@ -40,3 +33,4 @@ public class TextObjectGenarator : MonoBehaviour {
 	void Update () {
 	}
 }
+} // ARCamera
