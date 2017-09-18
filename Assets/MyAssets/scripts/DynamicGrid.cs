7abc613d3bfc7f3c3b5526d9665af2ecc3bbe2de
hayaken8112 <hayashi8112@gmail.com>
2017-09-18 16:01:21 +0900

    録画終了後に音がなるようにした

--- Assets/MyAssets/scripts/DynamicGrid.cs
+++ Assets/MyAssets/scripts/DynamicGrid.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 
 public class DynamicGrid : MonoBehaviour {
 
-	public int col;
+	int col;
 	void Start () {
 		GameObject canvas = GameObject.Find("Canvas");
 		RectTransform parent = canvas.GetComponent<RectTransform>();
 		GridLayoutGroup grid = gameObject.GetComponent<GridLayoutGroup>();
+		col = grid.constraintCount;
 		float space = grid.spacing.x * (col-1) + grid.padding.left + grid.padding.right;
 
 		grid.cellSize = new Vector2((parent.rect.width-space)/col, (parent.rect.width-space)/col);
