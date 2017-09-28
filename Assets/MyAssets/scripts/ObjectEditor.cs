87a7c707a95d0c95308d4d61d7b43cded1c6bbc0
hayaken8112 <hayashi8112@gmail.com>
2017-09-29 00:18:59 +0900

    日本指スワイプで回転できるようになった

--- Assets/MyAssets/scripts/ObjectEditor.cs
+++ Assets/MyAssets/scripts/ObjectEditor.cs
@@ -10,9 +10,6 @@ using ARCamera;
 
 public class ObjectEditor : MonoBehaviour {
 
-	Vector3 dragStartPos;
-	Vector3 defaultObjRot;
-	Vector3 defaultObjScale;
 	GameObject lastARObject;
 	// Use this for initialization
 	void Start () {
@@ -23,6 +20,7 @@ public class ObjectEditor : MonoBehaviour {
 			// オブジェクトが増減すると、スライダーの位置を中心に戻す。
 			lastARObject = ARObj;
 		});
+
 		this.UpdateAsObservable().Where(_ => Input.touchCount == 2)
 		.Subscribe(_ => {
 			Touch touchZero = Input.GetTouch(0);
@@ -35,43 +33,22 @@ public class ObjectEditor : MonoBehaviour {
 			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
 			float deltaMagnitudediff = touchDeltaMag - prevTouchDeltaMag;
+
+			float aveDiff = (touchZero.deltaPosition.x + touchOne.deltaPosition.x)/2;
 			Debug.Log("diff1" + deltaMagnitudediff);
 				if (lastARObject != null) {
 					float scale = lastARObject.transform.localScale.x + deltaMagnitudediff * 0.001f;
-					if (scale < 0.005) return;
-					lastARObject.transform.localScale = new Vector3(scale, scale, scale);
+					if (scale > 0.005) {
+						lastARObject.transform.localScale = new Vector3(scale, scale, scale);
+					}
+					if (touchZero.deltaPosition.x * touchOne.deltaPosition.x > 0) {
+						Debug.Log("aveDiff:" + aveDiff);
+						Vector3 rot = lastARObject.transform.rotation.eulerAngles;
+						rot.y += aveDiff;
+						lastARObject.transform.rotation = Quaternion.Euler(rot.x, rot.y, rot.z);
+					}
 				}
 		});
 	}
-	public void MapPinch()
-    {
-       #if UNITY_EDITOR
-        var pinch = this.UpdateAsObservable().Select (pos_dist => Input.mousePosition.x);
-        var stop = this.UpdateAsObservable().Where(_ => Input.GetMouseButtonUp(0)).First();
-       #elif UNITY_IOS || UNITY_ANDROID
-		var pinch = this.UpdateAsObservable()
-			.Where(_ => Input.touchCount == 2)
-			.Select (pos_dist => Vector2.Distance(Input.GetTouch (0).position, Input.GetTouch (1).position));
-        var stop = this.UpdateAsObservable().Where (_ => Input.touchCount != 2).First();
-       #endif
-
-        IDisposable onPinch = pinch.Buffer(3)
-            .TakeUntil(stop)
-            .Subscribe(distanceParam => {
-				Debug.Log("pinch");
-            	float diff = distanceParam.Last() - distanceParam.First();
-
-              	#if UNITY_EDITOR
-			  	Debug.Log(diff);
-              	#elif UNITY_IOS || UNITY_ANDROID
-				if (lastARObject != null) {
-					float scale = lastARObject.transform.localScale.x + diff;
-					lastARObject.transform.localScale = new Vector3(scale, scale, scale);
-				}
-			    Debug.Log("diff" + diff);
-                #endif
-            });
-
-    }
 	
 }
