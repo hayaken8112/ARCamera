9e2624745021443f989bfb8d96f8782fe88928e6
hayaken8112 <hayashi8112@gmail.com>
2017-10-02 10:34:48 +0900

    Tutorial位置調整

--- Assets/MyAssets/scripts/CautionText.cs
+++ Assets/MyAssets/scripts/CautionText.cs
@@ -7,22 +7,31 @@ using UniRx;
 
 public class CautionText : MonoBehaviour {
 
-	ReactiveProperty<int> numOfPointCloud = new ReactiveProperty<int>();
+	ReactiveProperty<int> numOfPlane = new ReactiveProperty<int>();
+
+	private UnityARAnchorManager unityARAnchorManager;
 	// Use this for initialization
 	void Start () {
-		numOfPointCloud.Value = 0;
-        UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
-		numOfPointCloud.Subscribe(num => {
+		unityARAnchorManager = new UnityARAnchorManager();
+		numOfPlane.Value = 0;
+		numOfPlane.Subscribe(num => {
 			var textColor = this.gameObject.GetComponent<Text>().color;
-			textColor.a = num == 0 ? 1 : 0;
+			textColor.a = (num == 0) ? 1 : 0;
 			this.gameObject.GetComponent<Text>().color = textColor;
+			this.gameObject.GetComponentInChildren<RawImage>().color = textColor;
 		});
 	}
-    public void ARFrameUpdated(UnityARCamera camera)
-    {
-		if (camera.pointCloudData == null) return;
-        numOfPointCloud.Value = camera.pointCloudData.Length;
-    }
 
+	public void Update() {
+		numOfPlane.Value = unityARAnchorManager.GetCurrentPlaneAnchors().Count;
+	}
+
+
+
+
+        void OnDestroy()
+        {
+            unityARAnchorManager.Destroy ();
+        }
 	
 }
